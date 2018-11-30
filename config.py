71a3aeeaeb4692c9843d37f4af58c0eed4fde073
Max Krylow <mak.kryloff@ya.ru>
2018-11-30 17:43:59 +0300

    Add path to stockfish

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from sys import platform as _platform
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -12,3 +13,17 @@ SECRET_KEY = 'my precious'
 
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+
+
+if _platform == "linux" or _platform == "linux2":
+    # linux
+    pass
+elif _platform == "darwin":
+    # MAC OS X
+    STOCKFISH_PATH = "stockfish/Mac/"
+elif _platform == "win32":
+    # Windows
+    pass
+elif _platform == "win64":
+    # Windows 64-bit
+    pass
