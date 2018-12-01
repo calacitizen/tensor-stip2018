10638a8d53eaeb1a0c17a16714c19e3f1d244c26
Anton_Kurbatov <a.kurbatov@protonmail.com>
2018-12-01 15:58:03 +0000

    fix linux stockfish

--- config.py
+++ config.py
@@ -17,8 +17,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 if _platform == "linux" or _platform == "linux2":
     # linux
-    STOCKFISH_PATH = "stockfish_10_x64"
-    pass
+    STOCKFISH_PATH = "./stockfish_10_x64"
 elif _platform == "darwin":
     # MAC OS X
     STOCKFISH_PATH = "stockfish/Mac/stockfish-10-64"
